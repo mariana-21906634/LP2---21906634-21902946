2b5ee60033e98c97aeaeaa95e41801616432342b
mariana-21906634 <a21906634@alunos.ulht.pt>
2021-11-14 23:26:25 +0000

    createInitialBoard RESET

--- src/pt/ulusofona/lp2/deisiGreatGame/GameManager.java
+++ src/pt/ulusofona/lp2/deisiGreatGame/GameManager.java
@@ -2,16 +2,29 @@ package pt.ulusofona.lp2.deisiGreatGame;
 
 import javax.swing.*;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 
 public class GameManager {
 
     ArrayList<Programmer> programmers = new ArrayList<>();
-    int tamanhoTab;
+    ArrayList<Integer> jogada = new ArrayList<>();
+    int tamanhoTab, nrTurnos;
 
     public GameManager(){}
 
     public boolean createInitialBoard(String[][] playerInfo, int boardSize){
-        return false;
+        reset();
+        if(playerInfo.length < 2 || playerInfo.length > 4 || boardSize < playerInfo.length*2){
+            return false;
+        }
+        tamanhoTab = boardSize;
+        for (String[] player : playerInfo) {
+            programmers.add(new Programmer(Integer.parseInt(player[0]), player[1], player[2], player[3]));
+            jogada.add(Integer.parseInt(player[0]));
+        }
+        Collections.sort(jogada);
+        return true;
     }
 
     public String getImagePng(int position){
@@ -63,4 +76,10 @@ public class GameManager {
     public JPanel getAuthorsPanel(){
         return null;
     } //FEITO
+
+    public void reset() {
+        programmers.clear();
+        jogada.clear();
+        nrTurnos = 1;
+    }
 }
