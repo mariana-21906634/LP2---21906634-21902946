625761357726bb8e9dd9b6f27fde252b4deba834
Diogo Pereira <diogomfpereira13@gmail.com>
2022-02-04 01:23:01 +0000

    Functions.kt2.0

--- src/pt/ulusofona/lp2/deisiGreatGame/Functions.kt
+++ src/pt/ulusofona/lp2/deisiGreatGame/Functions.kt
@@ -12,17 +12,25 @@ fun router() : (CommandType) -> (GameManager,List<String>) -> String?{
 fun escolhaComando(commandType: CommandType):(GameManager,List<String>) -> String?{
     when(commandType){
         GET -> return{game, values -> functionGet(game, values)}
-        POST -> return{game, values -> functionGet(game, values)}
+        POST -> return{game, values -> functionPost(game, values) }
     }
 }
 
 fun functionGet(game:GameManager, list: List<String>): String?{
     when (list[0]) {
         "PLAYER" -> return player(game,list)
-        "PLAYER_BY_LANGUAGE" -> return playerByLanguage(game,list)
+        "PLAYERS_BY_LANGUAGE" -> return playerByLanguage(game,list)
         "POLYGLOTS" -> return polyglots(game)
         "MOST_USED_POSITIONS" -> return mostUsedPositions(game, list)
         "MOST_USED_ABYSSES" -> return mostUsedAbysses(game, list)
+    }
+    return null
+}
+
+fun functionPost(game:GameManager, list: List<String>): String?{
+    when (list[0]) {
+        "MOVE" -> return player(game,list)
+        "ABYSS" -> return playerByLanguage(game,list)
     }
     return null
 }
